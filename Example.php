9053f97877f19fd841248bcc4bf5c0d9ec1327a7
mworwa <marcin.worwa@sts.pl>
2021-03-02 20:40:39 +0100

    Constructor property promotion

--- Example.php
+++ Example.php
@@ -4,15 +4,15 @@ declare(strict_types=1);
 
 class Example
 {
-    public static function createUser(string $username, int $depositBalance = 20, $bonusBalance = 100): string
-    {
-        return sprintf(
-            'User %s created! Deposit balance = %d, Bonus balance = %d',
-            $username,
-            $depositBalance,
-            $bonusBalance
-        );
-    }
+    public function __construct(
+        private int $id,
+        private string $name,
+        private int $depositBalance,
+    ) {}
 }
 
-echo Example::createUser(username: 'Janusz666', bonusBalance: 300) . PHP_EOL;
+$example = new Example(1, 'Marcin', 666);
+
+var_dump($example);
+
+
