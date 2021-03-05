bf99b935853c65fe99e80fe0eecc6307fe9f23e5
mworwa <marcin.worwa@sts.pl>
2021-03-05 11:24:06 +0100

    String inspection functions

--- Example.php
+++ Example.php
@@ -2,28 +2,8 @@
 
 declare(strict_types=1);
 
-function handleGet(): string {
-    return 'Handling GET request';
-}
+$contains = str_contains('Foobar', 'Foo');
+$startsWith = str_starts_with('PHP 8.0', 'PHP');
+$endsWith = str_ends_with('PHP 8.0', '8.0');
 
-function handlePost(): string {
-    return 'Handling POST request';
-}
-$requestMethod = 'POST';
-
-$status = match ($requestMethod) {
-    'POST' => handlePost(),
-    'GET' => handleGet(),
-    default => throw new Exception('Unsupported HTTP method'),
-};
-
-var_dump($status);
-
-
-/*
-- Match expression moze zwracac wartosc
-- Dozwolone jest wiele wyrazeń 'POST', 'UPDATE' => handle()
-- Zostanie wykonany tylko jedna instrukcja podczas dopasowania wyrazenia
-- Wspierany jest default, ktory zostanie wykonany w przypadku niedopasowania do zdefiniowanych wyrazen
-- Match expression musi dopasować wyrażenie. W przypadku niedopasowania zostanie rzucony UnhandledMatchError
- */
+var_dump($contains, $startsWith, $endsWith);
