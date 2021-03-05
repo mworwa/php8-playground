b150b0f3c93bbdcf7df79ec87f296622fb99d2d2
mworwa <marcin.worwa@sts.pl>
2021-03-05 11:26:54 +0100

    Mixed pseudo type

--- Example.php
+++ Example.php
@@ -2,8 +2,10 @@
 
 declare(strict_types=1);
 
-$contains = str_contains('Foobar', 'Foo');
-$startsWith = str_starts_with('PHP 8.0', 'PHP');
-$endsWith = str_ends_with('PHP 8.0', '8.0');
-
-var_dump($contains, $startsWith, $endsWith);
+class Example
+{
+    public function foo(mixed $param): mixed
+    {
+        return $param;
+    }
+}
